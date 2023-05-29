f01b2091d9aa212258ca7644106e3d2fd9a15724
nstalways <nstalways@gmail.com>
2023-05-29 12:38:30 +0000

    fix: baseline & add evaluation

--- input/code/train.py
+++ input/code/train.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 import os.path as osp
 import time
 import math
@@ -19,6 +19,8 @@ from dataset import SceneTextDataset
 from model import EAST
 
 from utils import seed_everything, load_config, get_save_folder_name
+from inference import do_inference
+from deteval import calc_deteval_metrics
 
 
 def parse_args():
@@ -36,60 +38,45 @@ def parse_args():
     return args
 
 
-def do_training(seed, data_dir,
-                model_dir, image_size, input_size,
-                num_workers, batch_size, learning_rate,
-                max_epoch, save_interval, ignore_tags):
-    """모델을 학습시키는 함수입니다.
-
-    Args:
-        seed (_type_): 재현 가능성을 위한 seed setting에 사용되는 값입니다.
-        data_dir (_type_): 데이터가 저장되어 있는 디렉토리 경로입니다.
-        model_dir (_type_): 학습시킨 모델의 weights를 저장할 디렉토리 경로입니다. ex) ./trained_models/2023-05-26/base_config/
-        image_size (_type_): 원본 이미지를 resize할 때 사용하는 값입니다.
-        input_size (_type_): Random Crop을 적용할 때 사용하는 값입니다.
-        num_workers (_type_): 데이터를 불러올 때 사용할 CPU 코어 개수입니다.
-        batch_size (_type_): training batch size입니다.
-        learning_rate (_type_): optimizer의 learning rate입니다.
-        max_epoch (_type_): 최대 epoch을 의미합니다.
-        save_interval (_type_): 특정 간격으로 model weights를 저장합니다.
-        ignore_tags (_type_): 데이터에 존재하는 tag를 보고, 어떤 tag를 무시할 지 결정합니다.
-    """
+def do_training(config_path, settings, train, valid):
     # seed 세팅
-    seed_everything(seed)
+    seed_everything(settings['seed'])
 
     # dataset 생성
-    dataset = SceneTextDataset(
-        data_dir,
+    train_dataset = SceneTextDataset(
+        settings['data_dir'],
         split='train',
-        image_size=image_size,
-        crop_size=input_size,
-        ignore_tags=ignore_tags
+        image_size=train['image_size'],
+        crop_size=train['input_size'],
+        ignore_tags=settings['ignore_tags']
     )
 
     # dataloader 생성
-    dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
+    train_dataset = EASTDataset(train_dataset)
+    num_train_batches = math.ceil(len(train_dataset) / train['batch_size'])
     train_loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
+        train_dataset,
+        batch_size=train['batch_size'],
         shuffle=True,
-        num_workers=num_workers
+        num_workers=train['num_workers']
     )
 
     # device, model, optimizer, scheduler 설정
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
     model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=train['learning_rate'])
+    scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[train['max_epoch'] // 2], gamma=0.1)
 
-    # train loop
-    model.train()
-    for epoch in range(max_epoch):
+    best_hmean = float('-inf')
+
+    for epoch in range(train['max_epoch']):
         epoch_loss, epoch_start = 0, time.time()
         epoch_cls_loss, epoch_angle_loss, epoch_iou_loss = 0, 0, 0
-        with tqdm(total=num_batches) as pbar:
+        
+        # train loop
+        model.train()
+        with tqdm(total=num_train_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
                 pbar.set_description('[Epoch {}]'.format(epoch + 1))
 
@@ -114,47 +101,74 @@ def do_training(seed, data_dir,
         scheduler.step()
 
         print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
+            epoch_loss / num_train_batches, timedelta(seconds=time.time() - epoch_start)))
         
         # wandb logging - train
         wandb.log({
             "Epoch": epoch + 1,
-            "Total Loss": epoch_loss / num_batches,
-            "CLS Loss": epoch_cls_loss / num_batches,
-            "Angle Loss": epoch_angle_loss / num_batches,
-            "IoU Loss": epoch_iou_loss / num_batches
+            "Total Loss": epoch_loss / num_train_batches,
+            "CLS Loss": epoch_cls_loss / num_train_batches,
+            "Angle Loss": epoch_angle_loss / num_train_batches,
+            "IoU Loss": epoch_iou_loss / num_train_batches
         })
 
-        # TODO: train/val split과 evaluation 코드 구현하기
+        # validation
+        if (epoch + 1) % train['save_interval'] == 0:
+            print('Calculating DetEval Metrics on Validation Datasets...')
+            ufo_result_valid = do_inference(model=model, ckpt_fpath=None,
+                                            data_dir=settings['data_dir'],
+                                            input_size=valid['input_size'],
+                                            batch_size=valid['batch_size'],
+                                            split='val')
+            ufo_result_valid = ufo_result_valid['images']
+            
+            # validation을 하기 위한 형태로 predictions, gts를 변환
+            gt_bboxes_dict, pred_bboxes_dict = dict(), dict()
+            for image_name in ufo_result_valid.keys():
+                gt_bboxes_dict[image_name], pred_bboxes_dict[image_name] = [], []
 
+                # gt json load
+                with open(osp.join(settings['data_dir'], 'ufo/val.json'), 'r') as f:
+                    gt_anno = json.load(f)
+                
+                # convert format of ground truths to use deteval.py
+                gt_anno = gt_anno['images']
+                gt_words = gt_anno[image_name]['words'].values()
+                for gt_word in gt_words:
+                    gt_points = gt_word['points']
+                    gt_bboxes_dict[image_name].append(gt_points)
+                
+                # convert format of predictions to use deteval.py
+                pred_words = ufo_result_valid[image_name]['words'].values()
+                for pred_word in pred_words:
+                    pred_points = pred_word['points']
+                    pred_bboxes_dict[image_name].append(pred_points)
 
-        # save model
-        if (epoch + 1) % save_interval == 0:
-            if not osp.exists(model_dir):
-                os.makedirs(model_dir)
+            # validation data에 대한 deteval 계산
+            results = calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, verbose=True)
+            precision, recall, hmean = results['total'].values()
+            print('Done!')
 
-            curr_epoch, total_epoch = str(epoch+1).zfill(4), str(max_epoch).zfill(4)
-            model_name = '[' + curr_epoch + '|' + total_epoch + '].pth' # save_interval 구간일 때, 현재 에폭을 기준으로 이름을 지정합니다. e.g., [0005|0150].pth
-            ckpt_fpath = osp.join(model_dir, model_name)
-            torch.save(model.state_dict(), ckpt_fpath)
+            print('-'*52)
+            print('F1 Score: {:.4f} | Precision: {:.4f} | Recall: {:.4f}'.format(
+                hmean, precision, recall))
+            print('-'*52)
+            
+            # wandb logging - validation
+            wandb.log({
+                "Epoch": epoch + 1,
+                "F1 Score": hmean,
+                "Precision": precision,
+                "Recall": recall,
+            })
 
-
-def main(config_path, settings, training, evaluation):
-    """do_training을 호출하는 함수입니다. 코드의 가독성을 높이기 위해 사용합니다.
-
-    Args:
-        config_path (_type_): 학습한 모델의 weights를 저장하는 폴더의 이름을 설정하기 위해, configs 폴더 아래에 있는 yaml 파일의 이름을 사용합니다.
-        settings (_type_): 실험 환경을 setting하기 위한 값들이 담겨있는 dictionary입니다. 사용자(who), seed 값이 담겨있습니다.
-        training (_type_): 학습 시 사용할 값들이 담겨있는 dictionary입니다.
-        evaluation (_type_): 평가 시 사용할 값들이 담겨있는 dictionary입니다.
-                             현재 버전(23.05.25)으로는 train/val split이 안 되어 있기 때문에, 아무 값도 담겨있지 않습니다.
-    """
-    model_dir = osp.join(training['model_dir'], get_save_folder_name(config_path))
-    
-    do_training(settings['seed'], training['data_dir'],
-                model_dir, training['image_size'], training['input_size'],
-                training['num_workers'], training['batch_size'], training['learning_rate'],
-                training['max_epoch'], training['save_interval'], training['ignore_tags'])
+            # saveF1 score가 갱신되는 경우 모델을 새로 저장
+            if hmean > best_hmean:
+                print(f'update best model... before: {best_hmean}, after: {hmean}')
+                ckpt_fpath = osp.join(settings['model_dir'], get_save_folder_name(config_path)) + '.pth'
+                torch.save(model.state_dict(), ckpt_fpath)
+                best_hmean = hmean
+                print('Done!')
 
 
 if __name__ == '__main__':
@@ -174,16 +188,17 @@ if __name__ == '__main__':
 
     # 불러온 yaml 파일을 사용하기 편리하도록 분리하기
     settings = cfgs['settings']
-    training, evaluation, _ = cfgs['training'], cfgs['evaluation'], cfgs['inference']
+    train, valid, _ = cfgs['train'], cfgs['valid'], cfgs['test']
 
     # wandb 실험 이름 설정
-    run_name = f"{settings['who']}_{training['max_epoch']}_{training['input_size']}" # e.g., sy_150_1024 (계속 같은 모델을 사용하니 모델 이름은 제외했어요)
+    run_name = f"{settings['who']}_{train['max_epoch']}_{train['input_size']}" # e.g., sy_150_1024 (계속 같은 모델을 사용하니 모델 이름은 제외했어요)
     wandb.run.name = run_name
 
     # 예외 처리
-    if training['input_size'] % 32 != 0:
+    if train['input_size'] % 32 != 0:
         raise ValueError('`input_size` must be a multiple of 32')
 
     # 실험 시작!
-    main(args.config_path, settings, training, evaluation)
+    do_training(args.config_path, settings, train, valid)
+    # main(args.config_path, settings, train, valid)
     
--- input/code/utils.py
+++ input/code/utils.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 
 import yaml
-from datetime import datetime
 import os.path as osp
 import random
 
@@ -22,20 +21,18 @@ def seed_everything(seed:int=42):
 
 
 def get_save_folder_name(yaml_path) -> str:
-    """파일을 '날짜/yaml 파일의 이름/' 방식으로 폴더에 저장할 때, 폴더명을 만들어주는 함수입니다.
+    """파일을 'yaml 파일의 이름/' 방식으로 폴더에 저장할 때, 폴더명을 만들어주는 함수입니다.
 
     Args:
         yaml_path (_type_): yaml의 경로입니다. e.g., ./configs/sy/01_sy_300_1024.yaml
 
     Returns:
-        str: "2023-05-25/01_sy_300_1024"와 같이 폴더명으로 사용할 문자열 반환
+        str: "01_sy_300_1024/"와 같이 폴더명으로 사용할 문자열 반환
     """
 
-    today = datetime.now()
-    date_folder = f"{today.year}-{today.month}-{today.day}"
-    yaml_folder = yaml_path.split('/')[-1].split('.')[0]
+    yaml_folder_name = yaml_path.split('/')[-1].split('.')[0]
 
-    return osp.join(date_folder, yaml_folder)
+    return yaml_folder_name
 
 
 def load_config(config_file):
